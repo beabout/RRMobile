b9e1663f19a68a3cefd58986ec8ec39ba6746213
Clay Beabout <beaboutclayton@gmail.com>
2021-07-08 22:57:55 -0500

    modified setup script

--- setup-env.sh
+++ setup-env.sh
@@ -2,9 +2,18 @@
 
 # follow https://reactnative.dev/docs/environment-setup
 
+# reminder to install XCode if on Mac
+if [ $(uname) = "Darwin" ]
+then
+    echo "hey, you're on mac. Make sure you have XCode installed."
+    echo "If you have it installed, add command line tools from "
+    echo "this tutorial: https://reactnative.dev/docs/environment-setup"
+fi
+
 # JS core 
 brew install node;
 # Facebook run file watch program
 brew install watchman;
 
-
+# install cocoapods 
+sudo gem install cocoapods
